5f6a7839a8b179306e72370352a3161e6fdbb830
delfinabenavidez <139728386+delfinabenavidez@users.noreply.github.com>
2023-11-24 23:44:49 -0300

    Actualizar navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="index.html">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Cart</a>
