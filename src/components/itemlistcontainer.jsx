089b1521447f5836f4424e54034f93ab22d8d53e
delfinabenavidez <139728386+delfinabenavidez@users.noreply.github.com>
2023-11-24 23:44:19 -0300

    Actualizar itemlistcontainer.jsx

--- src/components/itemlistcontainer.jsx
+++ src/components/itemlistcontainer.jsx
@@ -5,7 +5,6 @@ const ItemListContainer = () => {
   return (
     <div className="container mt-4">
       <h1>Welcome to our E-commerce store!</h1>
-      {/* Add your item list or other content here */}
     </div>
   );
 };
